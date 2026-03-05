b5e916ccf180aa1d504914c3e98fe6ac8dd49c64
muggle <tuilanya2006@gmail.com>
2026-03-05 08:52:23 +0700

    btth3 completed

---
+++ btth3/main.js
@@ -0,0 +1,10 @@
+function KiemTraTuoi(age) {
+    if (age >= 18) {
+        console.log("Đủ 18 tuổi");
+    } else {
+        console.log("Chưa đủ 18 tuổi");
+    }
+}
+
+KiemTraTuoi(16);
+KiemTraTuoi(20);
